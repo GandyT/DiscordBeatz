27eb72040c03573e7e6f47f5c2f399f382690f74
culinaryAmphibian <teperbudget@gmail.com>
2021-07-01 11:13:21 -0400

    async/await

--- commands/play.js
+++ commands/play.js
@@ -1,5 +1,5 @@
 const levelPlay = require("../constantListeners/levelPlay.js");
-const Config = require("../resource/modules/config.js");
+const { PREFIX } = require("../resource/modules/config.json");
 const Fs = require("fs");
 
 module.exports = {
@@ -10,30 +10,25 @@ module.exports = {
         if (!args[1]) return message.channel.send("Please specify a song name");
         if (!message.channel.name.startsWith("discordbeatz")) return message.channel.send("Please create a channel called \"discordbeatz\" in order to play");
 
-        var songName = args.slice(1).join(" ");
+        var songName = args.slice(1).join(" ").toLowerCase();
 
-        if (!message.member.voice.channel) return message.channel.send("You must be in a voice channel to play");
+        let memberVc = message.member.voice?.channel;
+        if (!memberVc) return message.channel.send("You must be in a voice channel to play");
+        if (!message.guild.me.permissionsIn(memberVc).toArray().includes('SPEAK')) return message.channel.send('i don\'t have the permission to speak in that voice channel.')
 
-        var path = `C:/Users/phone/OneDrive/Desktop/DiscordBeatz/resource/assets/${songName}/${songName}.mp3`;
+        var path = `./resource/assets/${songName}/${songName}.mp3`;
         var songExists = Fs.existsSync(path);
 
-        if (!songExists) return message.channel.send(`That song does not exist. Type ${Config.PREFIX}songlist to find a list of songs`);
+        if (!songExists) return message.channel.send(`That song does not exist. Type ${PREFIX}songlist to find a list of songs`);
 
-        message.channel.send("Testing Latency...")
-            .then(latencyMsg => {
-                var latency = Date.now() - latencyMsg.createdTimestamp;
-                message.channel.send(`Latency is ${latency}ms`);
-                message.member.voice.channel.join()
-                    .then(connection => {
-                        levelPlay.add(message, songName, latency);
-                        connection.play(path)
-                            .on("finish", () => {
-                                connection.disconnect();
-                                levelPlay.forceFinish(message.author.id);
-                            });
-                    });
-            });
-
-
+        let latencyMsg = await message.channel.send("Testing Latency...")
+        var latency = Date.now() - latencyMsg.createdTimestamp;
+        latencyMsg.edit(`Latency is ${latency}ms`);
+        let connection = await message.member.voice.channel.join();
+        levelPlay.add(message, songName, latency);
+        connection.play(path).on("finish", () => {
+            connection.disconnect();
+            levelPlay.forceFinish(message.author.id);
+        });
     }
 }
