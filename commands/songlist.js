a48782b79e913c05e5ed627bb809dea6cd49ecac
culinaryAmphibian <teperbudget@gmail.com>
2021-07-01 11:41:35 -0400

    show song length

--- commands/songlist.js
+++ commands/songlist.js
@@ -1,17 +1,24 @@
 const Discord = require("discord.js");
 const Fs = require("fs");
 
-module.exports = {
-    names: ["songlist"],
-    async execute(Env) {
-        const { message, client, args } = Env;
+function songLength(name)
+{
+    let songBeats = JSON.parse(Fs.readFileSync(`./resource/assets/${name}/beats.json`));
+    return `${name} - ${Math.floor((songBeats[0].time - songBeats.pop().time)/1000)} seconds`
+}
 
-        Fs.readdir("C:/Users/phone/OneDrive/Desktop/DiscordBeatz/resource/assets", (err, files) => {
-            message.channel.send(
-                new Discord.MessageEmbed()
-                    .setTitle("**SONGS**")
-                    .setDescription(files.join("\n"))
-            );
-        });
+module.exports = {
+    names: ["songlist", 'songs'],
+    async execute(Env) {
+        const { message } = Env;
+
+        let dirs = Fs.readdirSync("./resource/assets");
+        let songList = [];
+        dirs.forEach(dir => songList.push(`${songLength(dir)}`));
+        message.channel.send(
+            new Discord.MessageEmbed()
+                .setTitle("**SONGS**")
+                .setDescription(songList.join('\n'))
+        );
     }
 }
