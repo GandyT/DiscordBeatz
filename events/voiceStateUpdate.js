3dab55167cb7e2e1634940191419740c83d90017
culinaryAmphibian <teperbudget@gmail.com>
2021-07-01 11:28:04 -0400

    misc stuff

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -3,12 +3,13 @@ const levelPlay = require("../constantListeners/levelPlay.js");
 module.exports = {
     event: "voiceStateUpdate",
     async execute(oldMember, newMember) {
-        if (newMember.channelID == null) {
+        if (!newMember.voice?.channelID) {
             // user left a vc
 
             levelPlay.forceFinish(newMember.id);
             var vc = newMember.guild.channels.cache.find(c => c.id == oldMember.channelID);
-            vc.leave();
+            // optional chaining to prevent errors in case the user left because the channel was deleted
+            vc?.leave();
         }
     }
 }
